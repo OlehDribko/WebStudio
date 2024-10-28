8931c73fa20bbea23d92a613ca31162b20d00f73
OlehDribko <131261254+OlehDribko@users.noreply.github.com>
2024-10-28 11:23:17 +0200

    new iteration 28.10.2024

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+
+function doStuff() {
+    // Тіло функції
+    console.log('Log inside multiply function');
+  }
+  
+  // Виклики функції multiply
+  doStuff(); // 'Log inside multiply function'
+  doStuff(); // 'Log inside multiply function'
+  doStuff(); // 'Log inside multiply function'
